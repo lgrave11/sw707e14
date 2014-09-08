00d22e26849902038db7e94f97de3b08850ba4b4
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2014-09-08 09:49:52 +0200

    Report initial

---
+++ Report/compile.sh
@@ -0,0 +1,6 @@
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape Main.tex
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape Main.tex
+bibtex Main.aux
+bibtex Main.aux
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape Main.tex
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape Main.tex
