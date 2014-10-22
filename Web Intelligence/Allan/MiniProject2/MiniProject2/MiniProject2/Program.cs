6a0a725e9b5aa0f95b707f55ed6de4aec726b5a9
Søren Skibsted Als <s_s_a_92@hotmail.com>
2014-10-22 15:34:48 +0200

    Als skriver en masse

---
+++ Web Intelligence/Allan/MiniProject2/MiniProject2/MiniProject2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Math;
+using Accord.Math.Decompositions;
+
+namespace MiniProject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] array = new double[9, 9] { { 2, -1, -1, 0, 0, 0, 0, 0, 0 }, { -1,2,-1,0,0,0,0,0,0 }, {-1,-1,4,-1,-1,0,0,0,0 }, {0,0,-1,4,-1,-1,-1,0,0 }, {0,0,-1,-1,4,-1,-1,0,0 },{0,0,0,-1,-1,4,-1,-1,0},{0,0,0,-1,-1,-1,4,-1,0},{0,0,0,0,0,-1,-1,3,-1},{0,0,0,0,0,0,0,-1,1} };
+
+            
+            var gevd = new EigenvalueDecomposition(array);
+            
+            for(int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(gevd.DiagonalMatrix[i, i]);
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    Console.Write(gevd.Eigenvectors[i, j] + " ");
+                }
+                Console.Write("\n");
+            }           
+
+            Console.Read();
+        }
+    }
+}
