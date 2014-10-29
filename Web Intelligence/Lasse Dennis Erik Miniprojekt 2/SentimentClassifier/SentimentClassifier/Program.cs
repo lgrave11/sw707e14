3d3e4ec20ea482dd95c62626f06aee0c5a759dc1
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2014-10-29 14:33:26 +0100

    Sentiment classifier for WI LED Miniproject 2

---
+++ Web Intelligence/Lasse Dennis Erik Miniprojekt 2/SentimentClassifier/SentimentClassifier/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SentimentClassifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parser parser = new Parser("SentimentTrainingData.txt");
+            //List<List<Review>> partitions = parser.getDataSets(10);
+            Console.WriteLine(HttpUtility.HtmlDecode("&amp;"));
+            Console.ReadLine();
+        }
+    }
+}
