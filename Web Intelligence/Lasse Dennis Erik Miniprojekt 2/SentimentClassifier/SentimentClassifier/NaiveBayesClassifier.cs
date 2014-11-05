27c17bb13789b869dbf9a410304b6f44f04f4e3c
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2014-11-05 13:10:52 +0100

    Sidste del af miniprojekt 2 kode.

--- Web Intelligence/Lasse Dennis Erik Miniprojekt 2/SentimentClassifier/SentimentClassifier/NaiveBayesClassifier.cs
+++ Web Intelligence/Lasse Dennis Erik Miniprojekt 2/SentimentClassifier/SentimentClassifier/NaiveBayesClassifier.cs
@@ -8,7 +8,7 @@ using System.Diagnostics;
 namespace SentimentClassifier
 {
     
-    class NaiveBayesClassifier
+    public class NaiveBayesClassifier
     {
         List<Review> LearningData { get; set; }
         int N; // N
--- Web Intelligence/Lasse Dennis Erik Miniprojekt 2/SentimentClassifier/SentimentClassifier/Review.cs
+++ Web Intelligence/Lasse Dennis Erik Miniprojekt 2/SentimentClassifier/SentimentClassifier/Review.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SentimentClassifier
 {
     [Serializable]
-    class Review
+    public class Review
     {
         public Review()
         {
